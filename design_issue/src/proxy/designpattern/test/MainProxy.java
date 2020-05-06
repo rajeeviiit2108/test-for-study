ea4d674a1113a7f352455e2968b31716c4d16249
er-rajeevrs <er.rajeevrs@gmail.com>
2020-05-06 13:14:07 +0530

    Test for new commit
    Signed-off-by: er-rajeevrs <er.rajeevrs@gmail.com>

--- design_issue/src/proxy/designpattern/test/MainProxy.java
+++ design_issue/src/proxy/designpattern/test/MainProxy.java
@@ -3,6 +3,7 @@ package proxy.designpattern.test;
 public class MainProxy {
 
 	public static void main(String[] args) {
+		System.out.println("In main proxy::::");
 		String fileName="test";
 		Image image=new ProxyImage(fileName);
 		image.displayImage();
